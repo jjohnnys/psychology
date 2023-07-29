9a31ac6ad2bd33bd406e8feb38080382561b176a
jjohnnys <johnnysilva85@gmail.com>
2023-07-29 15:46:54 -0300

    new project java spring

---
+++ psychological_care/src/main/java/br/com/jjohnys/psychological_care/PsychologicalCareApplication.java
@@ -0,0 +1,13 @@
+package br.com.jjohnys.psychological_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologicalCareApplication.class, args);
+	}
+
+}
---
+++ psychological_care/src/test/java/br/com/jjohnys/psychological_care/PsychologicalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jjohnys.psychological_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
